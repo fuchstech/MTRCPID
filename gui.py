e455c48ad2992e5c2363162d982ca442a28f2668
Kerem Yıldız <49817710+Passeportout@users.noreply.github.com>
2022-12-06 20:19:53 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -8,10 +8,14 @@ import numpy as np
 from pymavlink import mavutil
 from threading import Thread
 from time import sleep
-
-
-master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
-master.wait_heartbeat()
+from serial import SerialException
+global master
+try:
+    master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
+    master.wait_heartbeat()
+except SerialException:
+    print("Pixhawk is not connected")
+    sys.exit()
 connection = True
 print("Connected")
 data_imu = {
@@ -46,6 +50,25 @@ def get_data():
 get_imu = Thread(target=get_data)
 get_imu.start()
 
+foreground = cv2.imread('tilki.png')
+
+# position of logo on video
+top_left = [0, 0] # the top-left corner of your logo goes here
+tx = top_left[0] # less typing later
+ty = top_left[1]
+
+# crop of logo
+left = 0
+right = 100
+top = 0 # y = 0 is the top of the image
+bottom = 100
+
+# calculate width and height of logo crop
+width = right - left
+height = bottom - top
+
+# main loop
+alpha = 0.4
 
 #python3.8 -m pip install opencv-python==4.3.0.36
 class VideoThread(QThread):
@@ -54,25 +77,31 @@ class VideoThread(QThread):
     def run(self):
         # capture from web cam
         cap = cv2.VideoCapture(0)
-        posX = 0
+        posX = 5
         X1 = 100
         X2 = 540
         color = (39,237,250)
         while True:
             
             ret, cv_img = cap.read()
+            cv_slice = cv_img[ty:ty+height, tx:tx+width]; 
+            logo_slice = foreground[top:bottom, left:right]; 
+            added_image = cv2.addWeighted(cv_slice,alpha,logo_slice,1-alpha,0)
+            cv_img[ty:ty+height, tx:tx+width] = added_image
             #cv2.line(cv_img, (40,posX), (600,posX),(255,255,255),1)
             cv2.line(cv_img, (X1,30),(X1,450),color,2)
             cv2.line(cv_img, (X2,30),(X2,450),color,2)
-            cv2.line(cv_img, (40,posX+240), (620,posX+240),color,1)
-            for i in range(1,17):
+            cv2.line(cv_img, (540,posX+240), (640,posX+240),color,1)
+            cv2.line(cv_img, (0,posX+240), (100,posX+240),color,1)
+            times = 25
+            for i in range(17):
                 if i%3 == 1:
 
-                    cv2.line(cv_img, (X1,posX+i*30), (X1+30,posX+i*30),color,3)
-                    cv2.line(cv_img, (X2,posX+i*30), (X2-30,posX+i*30),color,3)
+                    cv2.line(cv_img, (X1,posX+i*times), (X1+30,posX+i*times),color,3)
+                    cv2.line(cv_img, (X2,posX+i*times), (X2-30,posX+i*times),color,3)
                 else:
-                    cv2.line(cv_img, (X1,posX+i*30), (X1+20,posX+i*30),color,2)
-                    cv2.line(cv_img, (X2,posX+i*30), (X2-20,posX+i*30),color,2) 
+                    cv2.line(cv_img, (X1,posX+i*times), (X1+20,posX+i*times),color,2)
+                    cv2.line(cv_img, (X2,posX+i*times), (X2-20,posX+i*times),color,2) 
                 
             if ret:
                 pitch = np.round(data_imu["pitch"]*100*2,1)
@@ -94,7 +123,6 @@ class App(QWidget):
         self.image_label.resize(self.disply_width, self.display_height)
         # create a text label
         self.textLabel = QLabel('Webcam')
-
         # create a vertical box layout and add the two labels
         vbox = QVBoxLayout()
         vbox.addWidget(self.image_label)
